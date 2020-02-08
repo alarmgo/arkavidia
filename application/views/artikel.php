d052d54ebfadb3bfa4dca5f84c599dd5822a7bde
Additya Rivaldo <addityarivaldo89@gmail.com>
2020-02-08 19:27:43 +0700

    Update artikel.php

---
+++ application/views/artikel.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" type="text/css" href="assets/res/datatables/jquery.dataTables.min.css">
+    <?php $this->load->view('_parts/resources.php') ?>
+    <title>Artikel KoncoTernak</title>
+    <style>
+        .font{
+            font-family : Sogeo UI;
+            font-weight: 100;
+        }
+    </style>
+</head>
+
+<body>
+    <table class="display" id="example">
+        <thead>
+            <tr>
+                <th><font>TERNAK SAPI DENGAN METODE PRANAKAN</font></th>
+            </tr>
+        </thead>
+        <tbody>
+                <tr>
+                    <td>Berternak sapi memanglah tidak cepat, tapi..</td>
+                    <td>|</td>
+                    <td><b><a href="">Lanjutkan Membaca</a></b></td>
+                </tr>
+        </tbody>
+        <thead>
+            <tr>
+                <th><font>10 FAKTA MENGENAI SAPI YANG SEDANG HAMIL</font></th>
+            </tr>
+        </thead>
+        <tbody>
+                <tr>
+                    <td>Jika Anda selama ini melakukan hal-hal dibawah, maka Anda harus..</td>
+                    <td>|</td>
+                    <td><b><a href="">Lanjutkan Membaca</a></b></td>
+                </tr>
+        </tbody>
+        <thead>
+            <tr>
+                <th><font>SAPI BETINA ANDA SAKIT? INI OBATNYA!</font></th>
+            </tr>
+        </thead>
+        <tbody>
+                <tr>
+                    <td>Saat sapi sedang sakit, biasanya akan ditandai dengan..</td>
+                    <td>|</td>
+                    <td><b><a href="">Lanjutkan Membaca</a></b></td>
+                </tr>
+        </tbody>
+        <thead>
+            <tr>
+                <th><font>TERNAK SAPI DENGAN METODE PRANAKAN</font></th>
+            </tr>
+        </thead>
+        <tbody>
+                <tr>
+                    <td>Berternak sapi memanglah tidak cepat, tapi.. </td>
+                    <td>|</td>
+                    <td><b><a href="">Lanjutkan Membaca</a></b></td>
+                </tr>
+        </tbody>
+        <thead>
+            <tr>
+                <th><font>10 FAKTA MENGENAI SAPI YANG SEDANG HAMIL</font></th>
+            </tr>
+        </thead>
+        <tbody>
+                <tr>
+                    <td>Jika Anda selama ini melakukan hal-hal dibawah, maka Anda harus..</td>
+                    <td>|</td>
+                    <td><b><a href="">Lanjutkan Membaca</a></b></td>
+                </tr>
+        </tbody>
+        <thead>
+            <tr>
+                <th><font>SAPI BETINA ANDA SAKIT? INI OBATNYA!</font></th>
+            </tr>
+        </thead>
+        <tbody>
+                <tr>
+                    <td>Saat sapi sedang sakit, biasanya akan ditandai dengan..</td>
+                    <td>|</td>
+                    <td><b><a href="">Lanjutkan Membaca</a></b></td>
+                </tr>
+        </tbody>
+    </table>
+</body>
+
+</html>
