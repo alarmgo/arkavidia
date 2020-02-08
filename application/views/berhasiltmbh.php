12661635417b81b3f9c87226788776dead6b3cec
Additya Rivaldo <addityarivaldo89@gmail.com>
2020-02-09 03:45:32 +0700

    kelar cuk

--- application/views/berhasiltmbh.php
+++ application/views/berhasiltmbh.php
@@ -5,7 +5,7 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
-	<title>Pembayaran Terkonfirmasi</title>
+	<title>Produk Ditambahkan</title>
 
 	<?php $this->load->view('_parts/resources.php') ?>
 </head>
