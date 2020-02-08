47b2c97472ad5257f714b64825a0984b31d41d1b
alarmgo <08889237023a@gmail.com>
2020-02-08 10:59:59 +0700

    bootstrap

---
+++ application/views/home.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>Document</title>
+
+	<?php $this->load->view('_parts/resources.php') ?>
+</head>
+
+<body>
+	<?php $this->load->view('_parts/navbar_guest.php') ?>
+
+</body>
+
+</html>
