d8bb678b05e6851b9490e684b8ad7d093905765b
alarmgo <08889237023a@gmail.com>
2020-02-08 12:33:01 +0700

    navbar

--- application/views/_parts/navbar_guest.php
+++ application/views/_parts/navbar_guest.php
@@ -1,20 +1,18 @@
 <nav class="navbar navbar-expand-lg navbar-light shadow-sm bg-konco">
-	<a class="navbar-brand" href="#"><img src="assets/images/Logo.png" alt="logo" style="height: 32px;"></a>
-	<button style="height: 20px;" class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+<button style="height: 20px;" class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 		<span class="navbar-toggler-icon"></span>
 		<div class="btn-group">
 			<button type="button" style=" font-size: 0.5em;" class="btn btn-konco dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
 				<img src="assets/images/icons/menu.png" style="height: 20px;"><br>Menu
 			</button>
 			<div class="dropdown-menu">
-				<a class="dropdown-item" href="#">Action</a>
-				<a class="dropdown-item" href="#">Another action</a>
-				<a class="dropdown-item" href="#">Something else here</a>
-				<div class="dropdown-divider"></div>
-				<a class="dropdown-item" href="#">Separated link</a>
+			<a class="dropdown-item" href="#">Artikel</a>
+				<a class="dropdown-item" href="#">Jual-Beli</a>
 			</div>
 		</div>
 	</button>
+	<a class="navbar-brand" href="#"><img src="assets/images/Logo.png" alt="logo" style="height: 32px;"></a>
+
 
 	<div class="collapse navbar-collapse" id="navbarSupportedContent">
 		<ul class="navbar-nav mr-auto">
