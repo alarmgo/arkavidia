5051db001043adced3d7b204d2d6b3db4872ff10
alarmgo <08889237023a@gmail.com>
2020-02-08 15:29:31 +0700

    sidebar admin

--- application/views/admin/index.php
+++ application/views/admin/index.php
@@ -2,69 +2,61 @@
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link href="assets/res/css/bootstrap.min.css" rel="stylesheet">
-    <title>Admin Konco Ternak</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<link href="assets/res/css/bootstrap.min.css" rel="stylesheet">
+	<title>Admin Konco Ternak</title>
+	<?php $this->load->view('_parts/resources.php') ?>
 </head>
 
 <body>
-    <nav class="navbar navbar-expand navbar-dark bg-success static-top">
+	<nav class="navbar navbar-expand navbar-dark bg-success fixed-top">
 
-        <a class="navbar-brand mr-1" href="<?php echo site_url('admin') ?>">KoncoTernak</a>
+		<a class="navbar-brand mr-1" href="<?php echo site_url('admin') ?>">KoncoTernak</a>
 
-        <button class="btn btn-link btn-sm text-white order-1 order-sm-0" id="sidebarToggle" href="#">
-            <i class="fas fa-bars"></i>
-        </button>
+		<button class="btn btn-link btn-sm text-white order-1 order-sm-0" id="sidebarToggle" href="#">
+			<i class="fas fa-bars"></i>
+		</button>
 
-        <!-- Navbar Search -->
-        <form class="d-none d-md-inline-block form-inline ml-auto mr-0 mr-md-3 my-2 my-md-0">
-        </form>
+		<!-- Navbar Search -->
+		<form class="d-none d-md-inline-block form-inline ml-auto mr-0 mr-md-3 my-2 my-md-0">
+		</form>
 
-        <!-- Navbar -->
-        <ul class="navbar-nav ml-auto ml-md-0">
+		<!-- Navbar -->
+		<ul class="navbar-nav ml-auto ml-md-0">
 
-            <li class="nav-item dropdown no-arrow">
-                <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    <i class="fas fa-user-circle fa-fw"></i> Admin
-                </a>
-                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="userDropdown">
-                    <a class="dropdown-item" href="#">Settings</a>
-                    <a class="dropdown-item" href="#">Activity Log</a>
-                    <div class="dropdown-divider"></div>
-                    <a class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">Logout</a>
-                </div>
-            </li>
-        </ul>
+			<li class="nav-item dropdown no-arrow">
+				<a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+					<i class="fas fa-user-circle fa-fw"></i> Admin
+				</a>
+				<div class="dropdown-menu dropdown-menu-right" aria-labelledby="userDropdown">
+					<a class="dropdown-item" href="#">Settings</a>
+					<a class="dropdown-item" href="#">Activity Log</a>
+					<div class="dropdown-divider"></div>
+					<a class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">Logout</a>
+				</div>
+			</li>
+		</ul>
 
-    </nav>
+	</nav>
 
-    <div class="container">
-        <div class="row">
-            <div class="col-1">
-                <ul class="sidebar navbar-nav">
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">
-                            <i class="fas fa-fw fa-users"></i>
-                            <span>Users</span></a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">
-                            <i class="fas fa-fw fa-users"></i>
-                            <span>Users</span></a>
-                    </li>
-                </ul>
-            </div>
-            <div class="col-11">
-                <div class="container-fluid">
-                    <h1 class="mt-4">Simple Sidebar</h1>
-                    <p>The starting state of the menu will appear collapsed on smaller screens, and will appear non-collapsed on larger screens. When toggled using the button below, the menu will change.</p>
-                    <p>Make sure to keep all page content within the <code>#page-content-wrapper</code>. The top navbar is optional, and just for demonstration. Just create an element with the <code>#menu-toggle</code> ID which will toggle the menu when clicked.</p>
-                </div>
-            </div>
-        </div>
-    </div>
+	<div class="container-fluid">
+		<div class="row mt-5" style="height: 100vh;">
+			<div class="col bg-dark px-0">
+				<button class="btn btn-dark text-center font-weight-light" style="width: 100%; height: 6vh; font-size: 1em;" href="#">User</button>
+				<button class="btn btn-dark text-center font-weight-light" style="width: 100%; height: 6vh; font-size: 1em;" href="#">Transaksi</button>
+				<button class="btn btn-dark text-center font-weight-light" style="width: 100%; height: 6vh; font-size: 1em;" href="#">About</button>
+
+
+
+			</div>
+			<div class="col-10 bg-light">
+
+			</div>
+		</div>
+	</div>
+
 </body>
 
 <script src="assets/res/jquery/jquery.min.js'"></script>
